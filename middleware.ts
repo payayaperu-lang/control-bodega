7f9bdea407c686a1cd488d4b948109b50bfea2b5
AxelSifuentes <axelion16@gmail.com>
2026-04-23 13:30:51 -0500

    Fix: Import NextRequest en middleware

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,19 @@
 import { createClient } from '@/utils/supabase/middleware'
+import { NextRequest } from 'next/server' // <--- AGREGA ESTA LÍNEA
 
 export function middleware(request: NextRequest) {
-  return createClient(request)
+   return createClient(request)
 }
 
 export const config = {
   matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
     '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
