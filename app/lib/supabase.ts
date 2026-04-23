49df1cc98070fd22724c9b113aef2697540f8852
AxelSifuentes <axelion16@gmail.com>
2026-04-23 13:46:15 -0500

    Corrigiendo ruta de supabase

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://creghvwvqszccsorghzn.supabase.co";
 
-const supabaseKey = "sb_publishable_vKhrxrsSgjY9NO0q0oIvEA_R5olYCde";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNyZWdodnd2cXN6Y2Nzb3JnaHpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3MzA3NjMsImV4cCI6MjA5MjMwNjc2M30.iqN2KpToCFVxjrC784WfPkeVuw6CzM7v73-xE8uNqOI";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
